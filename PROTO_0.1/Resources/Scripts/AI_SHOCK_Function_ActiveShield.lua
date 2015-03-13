2cab5a4591c1008886903f4f6e67d75b5a5be57b
Johann <jo.fabries@gmail.com>
2015-03-13 16:19:27 +0100

    Opti shoot

--- PROTO_0.1/Resources/Scripts/AI_SHOCK_Function_ActiveShield.lua
+++ PROTO_0.1/Resources/Scripts/AI_SHOCK_Function_ActiveShield.lua
@@ -8,7 +8,23 @@
 function AI_SHOCK.ActiveShield ( )
 --------------------------------------------------------------------------------
 	
-	object.setScale ( this.getObject ( ),this.nScale ( ),this.nScale ( ),this.nScale ( ) )
+	    if(this.bActive()) 
+    then 
+        if(this.nScale ( ) >2 )
+        then 
+            this.nWayFactor ( -1)
+             this.nIncrement ( 0.03)
+            
+        elseif (this.nScale ( ) <0.1)
+        then 
+            this.nWayFactor ( 0)
+            this.bActive ( false)
+           
+        end
+        
+        this.nScale ( this.nScale ( )+this.nIncrement ( )*this.nWayFactor())
+        object.setScale ( this.getObject ( ),this.nScale ( ),this.nScale ( ),this.nScale ( ) ) 
+    end
 	
 --------------------------------------------------------------------------------
 end
