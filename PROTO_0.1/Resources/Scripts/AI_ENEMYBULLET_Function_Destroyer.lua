c843394d94d9954149d7a10d21d3bf6635e6542b
Johann <jo.fabries@gmail.com>
2015-03-16 12:24:49 +0100

    Ajout enemy

---
+++ PROTO_0.1/Resources/Scripts/AI_ENEMYBULLET_Function_Destroyer.lua
@@ -0,0 +1,40 @@
+--------------------------------------------------------------------------------
+--  Function......... : Destroyer
+--  Author........... : 
+--  Description...... : 
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+function AI_ENEMYBULLET.Destroyer ( )
+--------------------------------------------------------------------------------
+	 
+    local hScene = application.getCurrentUserScene ( )
+    
+	-- GET & SET PLAYER OBJECT 
+    this.hParent( scene.getTaggedObject ( hScene,"PLAYER" ))
+    this.hCam( scene.getTaggedObject ( hScene,"CAM" )) 
+     
+    
+    -- CREATE COLLIDER FOR Y-AXIS
+    local notUsed, notUsed2, targetZ = camera.projectPoint ( this.hCam(), object.getTranslation ( this.hParent ( ), object.kGlobalSpace ) )
+
+    --Find xMax and yMin
+    local x, y, z = camera.unprojectPoint ( this.hCam(), 1, 1, targetZ ) 
+    local xMax = x 
+    local yMin = y 
+
+    --Find xMin and yMax
+    x, y, z = camera.unprojectPoint ( this.hCam(), -1, -1, targetZ ) 
+    local xMin = x 
+    local yMax = y 
+    
+    local bX,bY,notUsed3 = object.getTranslation ( this.getObject ( ),object.kGlobalSpace )
+    
+    if(bX > xMax or bX < xMin or bY < yMax or bY > yMin )
+    then 
+        scene.destroyRuntimeObject ( hScene, this.getObject ( ) )
+    end
+	
+--------------------------------------------------------------------------------
+end
+--------------------------------------------------------------------------------
